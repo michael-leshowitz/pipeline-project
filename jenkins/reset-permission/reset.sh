c8e9087f8772582b95417df3e3c8183e9696f1ab
jenkins <jenkins@localhost.localdomain>
2022-04-21 13:02:53 -0400

    removed it

--- jenkins/reset-permission/reset.sh
+++ jenkins/reset-permission/reset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "----Resetting target permissions----"
-docker exec -it -u root jenkins chown -R 1000:1000 /var/jenkins_home/workspace/pipeline-docker-maven/java-app/
+docker exec -u root jenkins chown -R 1000:1000 /var/jenkins_home/workspace/pipeline-docker-maven/java-app/
