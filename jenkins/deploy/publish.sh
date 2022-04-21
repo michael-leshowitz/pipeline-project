6db57bbca3df9e54b3c3dfc8c6b516b922004031
jenkins <jenkins@localhost.localdomain>
2022-04-21 09:48:43 -0400

    Added echo statements to publish

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -4,6 +4,10 @@ export IMAGE=$(sed -n '1p' /tmp/.auth)
 export TAG=$(sed -n '2p' /tmp/.auth)
 export PASS=$(sed -n '3p' /tmp/.auth)
 
+echo "Image: $IMAGE"
+echo "Tag: $TAG"
+echo "Pass: $PASS"
+
 docker login -u mleshowitz -p $PASS
 cd ~/maven && docker-compose up -d
 
